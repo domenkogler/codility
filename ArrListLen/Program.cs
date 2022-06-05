faddedb90d82f73c9e2abe3aaea531a4787a9411
Domen Kogler <domen.kogler@gmail.com>
2022-06-05 23:23:15 +0200

    add: ArrListLen

---
+++ ArrListLen/Program.cs
@@ -0,0 +1,18 @@
+﻿var s = new Solution();
+Console.WriteLine(s.solution(new int[] { 1, 4, -1, 3, 2 }));
+
+public class Solution
+{
+    public int solution(int[] A)
+    {
+        var count = 1;
+        var value = A[0];
+        for (var i = 0; i < A.Length; i++)
+        {
+            if (value == -1) return count;
+            value = A[value];
+            count++;
+        }
+        return -1;
+    }
+}
