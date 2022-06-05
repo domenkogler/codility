24476c5c6af298933758056f83c1e9bdc0f2bee0
Domen Kogler <domen.kogler@gmail.com>
2022-06-05 23:11:10 +0200

    add: SparseBinaryDecomposition

---
+++ SparseBinaryDecomposition/Program.cs
@@ -0,0 +1,22 @@
+﻿var s = new Solution();
+s.Log(74901729);
+
+public class Solution
+{
+    public void Log(int N)
+    {
+        Write(N);
+        Write(solution(N));
+        Write(N-solution(N));
+
+        void Write(int n)
+        {
+            Console.WriteLine($"{n}: {Convert.ToString(n, 2)}".PadLeft(40));
+        }
+    }
+
+    public int solution(int N)
+    {
+        return N & 0x55555555;
+    }
+}
