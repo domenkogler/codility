39ea0bab791d5d15dc368fa81711fb1807b91d57
Domen Kogler <domen.kogler@gmail.com>
2022-06-06 03:30:02 +0200

    StrSymmetryPoint

---
+++ StrSymmetryPoint/Program.cs
@@ -0,0 +1,19 @@
+﻿var s = new Solution();
+Console.WriteLine(s.solution("ata"));
+Console.WriteLine(s.solution(""));
+Console.WriteLine(s.solution("x"));
+
+class Solution
+{
+    public int solution(string S)
+    {
+        if (S.Length == 1) return 0;
+        if (S.Length < 3 || S.Length % 2 == 0) return -1;
+        var midpoint = (S.Length - 1) / 2;
+        for (int i = 1; i <= midpoint; i++)
+        {
+            if (S[midpoint - i] != S[midpoint + i]) return -1;
+        }
+        return midpoint;
+    }
+}
