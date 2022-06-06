7294a828ce0fd589b519a027ac24be15cb8406b0
Domen Kogler <domen.kogler@gmail.com>
2022-06-06 03:13:43 +0200

    add FirstUnique

---
+++ FirstUnique/Program.cs
@@ -0,0 +1,32 @@
+﻿var s = new Solution();
+Console.WriteLine(s.solution(new[] { 6,4,4,6 }));
+Console.WriteLine(s.solution(new[] { 1, 4, 3, 3, 1, 2 }));
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        var dic = new Dictionary<int, bool>();
+        for (int i = 0; i < A.Length; i++)
+        {
+            var value = A[i];
+            if (dic.ContainsKey(value))
+            {
+                dic[value] = true;
+            }
+            else
+            {
+                dic[value] = false;
+            }
+        }
+
+        try
+        {
+            return dic.First(kvp => !kvp.Value).Key;
+        }
+        catch (InvalidOperationException e)
+        {
+            return -1;
+        }
+    }
+}
