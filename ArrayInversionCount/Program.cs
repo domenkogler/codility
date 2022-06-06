c111eebad5d89c8731583c92c93b036fbad40203
Domen Kogler <domen.kogler@gmail.com>
2022-06-06 05:48:54 +0200

    DisappearingPairs

---
+++ ArrayInversionCount/Program.cs
@@ -0,0 +1,21 @@
+﻿var s = new Solution();
+Console.WriteLine(s.solution(new[] { -1, 6, 3, 4, 7, 4 }));
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        int length = A.Length;
+        var count = 0;
+        for (int i = 0; i < length; i++)
+        {
+            var value = A[i];
+            for (int j = i+1; j < length; j++)
+            {
+                if (value > A[j]) count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ DisappearingPairs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+using System.Text;
+
+var s = new Solution();
+Console.WriteLine(s.solution("ABCBBCBA"));
+Console.WriteLine(s.solution("ACCAABBC"));
+Console.WriteLine(s.solution("AAA"));
+Console.WriteLine(s.solution("BBBB"));
+Console.WriteLine(s.solution("CCCCC"));
+
+class Solution
+{
+    public string solution(string S)
+    {
+        if (S.Length < 2) return S;
+        var stack = new Stack<char>();
+        foreach (var c in S)
+        {
+            if (stack.Count == 0)
+            {
+                stack.Push(c);
+                continue;
+            }
+            if (stack.Peek() == c)
+            {
+                stack.Pop();
+                continue;
+            }
+            stack.Push(c);
+        }
+
+        return new string(stack.Reverse().ToArray());
+    }
+}
