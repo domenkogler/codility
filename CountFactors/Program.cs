f706f363cb6983ce955f33e283ef5e409225f068
Domen Kogler <domen.kogler@gmail.com>
2022-06-06 11:12:47 +0200

    MinPerimeterRectangle

---
+++ CountFactors/Program.cs
@@ -0,0 +1,20 @@
+﻿var s = new Solution();
+Console.WriteLine(s.solution(24));
+
+class Solution
+{
+    public int solution(int N)
+    {
+        if (N == 1) return 1;
+        var root = Math.Sqrt(N);
+        var count = 2;
+        var current = 2;
+        while (current <= root)
+        {
+            if (Math.Abs(current - root) < 0.0000000001) return ++count;
+            if (N % current == 0) count += 2;
+            current++;
+        }
+        return count;
+    }
+}
---
+++ MinPerimeterRectangle/Program.cs
@@ -0,0 +1,23 @@
+﻿var s = new Solution();
+Console.WriteLine(s.solution(1));
+
+class Solution
+{
+    public int solution(int N)
+    {
+        if (N == 1) return 4;
+        var root = Math.Sqrt(N);
+        var current = (int)root;
+        while (current >= 1)
+        {
+            if (N % current == 0)
+            {
+                var a = N / current;
+                var b = N / a;
+                return 2 * (a + b);
+            }
+            current--;
+        }
+        return -1;
+    }
+}
